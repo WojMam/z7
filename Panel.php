1376ff45328956d3b7d8b1649b0a7bed6a171c63
Wojciech Mamys <woj.mam@gmail.com>
2016-12-04 16:40:53 +0100

    Fixed LoggedIn status checker

--- Panel.php
+++ Panel.php
@@ -1,47 +1,51 @@
 <?php
 $user = $_COOKIE['user'];
 $pass =$_COOKIE['pass'];
-$logged_in =$_COOKIE['logged_in'];
+//$logged_in =$_COOKIE['logged_in'];
 
-if($logged_in = FALSE)
+if(!isset($_COOKIE['logged_in']))
 {
 		header('Location: http://serwer1615599.home.pl/z7/index.html');
 		exit();
 }
+else
+{
 ?>
 
-<HTML>
+	<HTML>
 
-	<HEAD>
-		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-		<title>Wojciech Mamys</title>
-		<link rel="stylesheet" href="style.css">
-		<link href="https://fonts.googleapis.com/css?family=Lato&subset=latin-ext" rel="stylesheet">
-	</HEAD>
-	
-	<BODY>
-	
-		<div class="flex-container">
-			Zalogowano jako <?php echo $user ?><br>
-			<br><form action="logout.php">
-				<input type="submit" value="Wyloguj" />
-			</form>
-		</div>
+		<HEAD>
+			<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+			<title>Wojciech Mamys</title>
+			<link rel="stylesheet" href="style.css">
+			<link href="https://fonts.googleapis.com/css?family=Lato&subset=latin-ext" rel="stylesheet">
+		</HEAD>
+		
+		<BODY>
+		
+			<div class="flex-container">
+				Zalogowano jako <?php echo $user ?><br>
+				<br><form action="logout.php">
+					<input type="submit" value="Wyloguj" />
+				</form>
+			</div>
 
-		<div class="flex-container">
-			<div class="flex-item" style="width: 20%;">
-				Wyúlij plik <br>
-				
+			<div class="flex-container">
+				<div class="flex-item" style="width: 20%;">
+					Wy≈õlij plik <br>
+					
+				</div>
 			</div>
-		</div>
-		
-		<div class="flex-container">
-			<div class="flex-item"  style="width: 90%;">
-				Twoje pliki: <br>
-				
+			
+			<div class="flex-container">
+				<div class="flex-item"  style="width: 90%;">
+					Twoje pliki: <br>
+					
+				</div>
 			</div>
-		</div>
+			
+		</BODY>
 		
-	</BODY>
-	
-</HTML>
+	</HTML>
+
+<?php } ?>
